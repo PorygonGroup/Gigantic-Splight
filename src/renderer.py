efd1cf37bdee8b0c82185397783d4561472cb5dc
XOR-op <17672363+XOR-op@users.noreply.github.com>
2022-05-26 13:25:30 +0800

    intuitive movement

--- src/renderer.py
+++ src/renderer.py
@@ -11,39 +11,38 @@ class Renderer:
         self.window = ti.ui.Window("Render particles", (800, 800), vsync=True)
         self.canvas = self.window.get_canvas()
         self.scene = ti.ui.Scene()
-        self.camera_pos = np.array([0, 0.5, 2], dtype=np.float64)
-        self.camera_dir = np.array([0, 0, -1], dtype=np.float64)
+        self.camera_pos = np.array([0.5, -1, 2], dtype=np.float64)
+        self.camera_dir = np.array([0.5, 1, 0], dtype=np.float64)
         camera = ti.ui.make_camera()
-        camera.position(0, 0.5, 2)
-        camera.lookat(1, 0, 0)
-        camera.fov(90)
-        self.scene.set_camera(camera)
         self.camera = camera
+        self.updateCamera()
 
     def updateCamera(self):
-        # TODO: make movement more intuitive
         self.window.get_event()
         POS_EPS = 0.01
         pos_delta = np.array([0.0, 0.0, 0.0])
         if self.window.is_pressed('w'):
-            pos_delta[0] += POS_EPS
-        if self.window.is_pressed('s'):
-            pos_delta[0] -= POS_EPS
-        if self.window.is_pressed('a'):
             pos_delta[1] += POS_EPS
-        if self.window.is_pressed('d'):
+        if self.window.is_pressed('s'):
             pos_delta[1] -= POS_EPS
+        if self.window.is_pressed('a'):
+            pos_delta[0] -= POS_EPS
+        if self.window.is_pressed('d'):
+            pos_delta[0] += POS_EPS
         if self.window.is_pressed('q'):
             pos_delta[2] += POS_EPS
         if self.window.is_pressed('e'):
             pos_delta[2] -= POS_EPS
 
         if self.window.is_pressed(ti.GUI.SHIFT):
-            self.camera_pos += pos_delta
-        else:
+            # only move angle
             self.camera_dir += pos_delta
-        self.camera.position(self.camera_pos[0], self.camera_pos[1], self.camera_pos[2])
-        self.camera.lookat(self.camera_dir[0], self.camera_dir[1], self.camera_dir[2])
+        else:
+            self.camera_pos += pos_delta
+            # keep direction the same
+            self.camera_dir += pos_delta
+        self.camera.position(*self.camera_pos)
+        self.camera.lookat(*self.camera_dir)
         self.camera.fov(90)
         self.scene.set_camera(self.camera)
 
