42d6dbea42cfdcb8be74ae4dd17af27e0bd59105
XOR-op <17672363+XOR-op@users.noreply.github.com>
2022-06-08 19:19:11 +0800

    fix scene

--- src/scene.py
+++ src/scene.py
@@ -3,4 +3,7 @@ import taichi as ti
 @ti.data_oriented
 class Scene:
     def __init__(self):
-        self.board_states = ti.Vector.field(3, float)
+        self.board_states = ti.Vector.field(3, float,shape=())
+
+    def update(self):
+        pass
